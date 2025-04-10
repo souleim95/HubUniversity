1bbd220eccc5acf6285e909895cbff14d42b548f
delsucflorian <delsuc.florian@gmail.com>
2025-04-10 23:13:27 +0200

    Suppression de alice et martin étant inutiles, et de la reinitialisation de la bdd

--- database/init.sql
+++ database/init.sql
@@ -1,27 +1,19 @@
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS role;
-
-CREATE TABLE role (
+CREATE TABLE IF NOT EXISTS role (
   idRole SERIAL PRIMARY KEY,
   nomRole VARCHAR(100) NOT NULL
 );
 
 INSERT INTO role (nomRole) VALUES
-  ('eleve'),      -- Accès aux modules publics (ex: Information)
-  ('professeur'), -- Niveau débutant/intermédiaire (ex: Visualisation Modification partiel)
-  ('directeur');  -- Niveau expert (ex: tous les droits)
+  ('eleve'),
+  ('professeur'),
+  ('directeur');
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
   email VARCHAR(100) UNIQUE NOT NULL,
-  idRole INTEGER DEFAULT 1,  -- Par défaut, le rôle 'eleve'
+  idRole INTEGER DEFAULT 1,
   password VARCHAR(255) NOT NULL,
   created_at TIMESTAMP DEFAULT NOW(),
   FOREIGN KEY (idRole) REFERENCES role(idRole)
 );
-
-INSERT INTO users (name, email, idRole, password) VALUES 
-  ('Alice Dupont', 'alice.dupont@example.com', 2, 'defaut'),
-  ('Bob Martin', 'bob.martin@example.com', 2, 'defaut');
-
